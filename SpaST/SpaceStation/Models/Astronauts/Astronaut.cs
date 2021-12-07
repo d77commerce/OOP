2c8c23168162df09b9c28fe9cd7a5979f3c54fab
Danail Tsachev <73217857+d77commerce@users.noreply.github.com>
2021-12-07 01:10:47 +0000

    new

---
+++ SpaST/SpaceStation/Models/Astronauts/Astronaut.cs
@@ -0,0 +1,57 @@
+﻿using SpaceStation.Models.Astronauts.Contracts;
+using SpaceStation.Models.Bags.Contracts;
+using SpaceStation.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceStation.Models.Astronauts
+{
+    public abstract class Astronaut : IAstronaut
+    {
+        private string name;
+        private double oxygen;
+
+        protected Astronaut(string name, double oxygen)
+        {
+            Name = name;
+            Oxygen = oxygen;
+        }
+
+        public string Name
+        {
+            get => name;
+           private set
+            {
+                if (value == " " && value == null)
+                {
+                    throw new ArgumentNullException(nameof(name), ExceptionMessages.InvalidAstronautName);
+                }
+                name = value;
+            }
+        }
+
+        public double Oxygen
+        {
+            get => oxygen;
+            set
+            {
+                if(value <= 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidOxygen);
+                }
+                oxygen = value;
+            }
+
+        }
+
+        public bool CanBreath => this.Oxygen > 0;
+
+        public IBag Bag { get; set; }
+
+        public void Breath()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
