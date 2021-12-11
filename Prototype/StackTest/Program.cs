f41c0c0a1bafc90348cacfde6b11498c4846b655
Danail Tsachev <73217857+d77commerce@users.noreply.github.com>
2021-12-11 19:22:02 +0000

    new

---
+++ Prototype/StackTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StackTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> stack = new Stack<char>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                stack.Push(input[i]);
+            }
+          while (stack.Peek()!=0)
+            {
+                Console.Write(stack.Pop());
+            }
+
+        }
+    }
+}
---
+++ Prototype/TestNull/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace TestNull
+{
+    class Person
+    {
+        private int? myVar;
+        private int age;
+        public int? NomOfKids
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
+
+
+        public int Age
+
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.NomOfKids = null;
+            Console.WriteLine((person.NomOfKids.HasValue) ? "have " : "No have");
+            Console.WriteLine(person.NomOfKids ?? 0);
+            Person person1 = new Person() { Age = 3 };
+            Person person2 = null;
+            Console.WriteLine((person1 ==null)? null : person1.Age);
+            Console.WriteLine(person1?.Age);
+            Console.WriteLine();
+            Console.WriteLine((person2 ==null)? null:person2.Age);
+            Console.WriteLine(person2?.Age);
+
+        }
+    }
+}
