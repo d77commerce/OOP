7340831c2a31b9583a9fc3ff3e02c3c822a41b14
Danail Tsachev <73217857+d77commerce@users.noreply.github.com>
2021-12-12 23:05:13 +0000

    new

--- Prototype/BasicStackOperations/Program.cs
+++ Prototype/BasicStackOperations/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BasicStackOperations
 {
@@ -6,7 +8,36 @@ namespace BasicStackOperations
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] inPut = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                  .Select(int.Parse).ToArray();
+            int NomberAdd = inPut[0];
+            int NomberRemuve = inPut[1];
+            int MajickNomber = inPut[2];
+            Stack<int> stack = new Stack<int>();
+            int[] nombers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                           .Select(int.Parse).ToArray();
+
+            if (NomberAdd <= NomberRemuve)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+            for (int i = 0; i < NomberAdd; i++)
+            {
+                stack.Push(nombers[i]);
+            }
+            for (int i = 0; i < NomberRemuve; i++)
+            {
+                stack.Pop();
+            }
+            if (stack.Contains(MajickNomber))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(stack.Peek());
+            }
         }
     }
 }
---
+++ Prototype/MaximumAndMinimumElement/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MaximumAndMinimumElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lineForRead = int.Parse(Console.ReadLine());
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < lineForRead; i++)
+            {
+                int[] line = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                int comand = line[0];
+                if (comand == 1)
+                {
+                    stack.Push(line[1]);
+
+                }
+                else if (comand == 2)
+                {
+                    if (stack.Count > 0)
+                    {
+                        stack.Pop();
+                    }
+
+                }
+                else if (comand == 3)
+                {
+                    if (stack.Count > 0)
+                    {
+                        Console.WriteLine(stack.Max());
+                    }
+                }
+                else if (comand == 4)
+                {
+                    if (stack.Count > 0)
+                    {
+                        Console.WriteLine(stack.Min());
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(", ", stack));
+        }
+    }
+}
